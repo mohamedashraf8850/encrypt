fbeb7b7e53bdf585a95ea112a3df1685a3a71c06
Muhammad Ashraf <mohamedashraf8850@gmail.com>
2019-09-24 11:51:41 +0200

    Update aes.dart

--- example/aes.dart
+++ example/aes.dart
@@ -1,17 +1,59 @@
 import 'package:encrypt/encrypt.dart';
+import 'package:flutter/material.dart';
 
-void main() {
-  final plainText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit';
+class aesPage extends StatefulWidget {
+  @override
+  _aesPageState createState() =>
+      _aesPageState();
+}
 
-  final key = Key.fromLength(32);
-  final iv = IV.fromLength(16);
-  final encrypter = Encrypter(AES(key));
+class _aesPageState
+    extends State<aesPage> {
+  TextEditingController PC = new TextEditingController();
 
-  final encrypted = encrypter.encrypt(plainText, iv: iv);
-  final decrypted = encrypter.decrypt(encrypted, iv: iv);
+  @override
+  void initState() {
+    super.initState();
+  }
 
-  print(decrypted);
-  print(encrypted.bytes);
-  print(encrypted.base16);
-  print(encrypted.base64);
+
+  encriptanddecrypt(){
+    final plainText = '${PC.text}';
+    final key = Keyo.fromLength(32);
+    final iv = IV.fromLength(16);
+    final encrypter = Encrypter(AES(key));
+
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
+    final decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+    print(decrypted);
+    print(encrypted.bytes);
+    print(encrypted.base16);
+    print(encrypted.base64);
+    setState((){
+      PC.text = encrypted.toString();
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            TextFormField(
+       controller: PC,
+              maxLines: 50,
+            ),
+            RaisedButton(
+              onPressed:encriptanddecrypt ,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+ 
 }
